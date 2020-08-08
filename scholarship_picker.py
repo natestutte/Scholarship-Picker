ff876b12099b7ef9ee9763908a76368cc7325423
Nate Stutte <ngs33@drexel.edu>
2020-08-08 18:03:17 -0400

    Moved commands to separate functions, format changes

--- scholarship_picker.py
+++ scholarship_picker.py
@@ -6,25 +6,91 @@
 # 
 # Repo : https://github.com/magfmur/Scholarship-Picker
 
+
 import random
 
 scholarships = []
 scholarshipspool = []
 scholarshipstrashpool = []
 
+
+# Command functions
+
+# choice_random : Picks a random scholarship from scholarshippool and adds it to scholarshiptrashpool
+# If there are no scholarships left in the pool, reset scholarshipspool and clear scholarshiptrashpool
+def choice_random():
+    global scholarshipspool
+    if len(scholarshipspool) == 0:
+            scholarshipspool = scholarshipstrashpool.copy()
+            scholarshipstrashpool.clear()
+            print("Reshuffling scholarship pool...")
+            
+    try:
+        scholarshippicked = random.choice(scholarshipspool)
+    except IndexError:
+        print("Error: List of scholarships is empty, add scholarships first!")
+    else:
+        scholarshipstrashpool.append(scholarshippicked)
+        scholarshipspool.remove(scholarshippicked)
+        for a in scholarships:
+            if a["Name"] == scholarshippicked:
+                print(scholarshippicked, "-", a["Link"])
+                break
+        else:
+            print(scholarshippicked)
+
+# choice_add : Add scholarships name and link
+# Must enter a unique scholarship with a non-empty link
+# Stores it in 'Scholarship.txt'
+def choice_add():
+    while True:
+        name = input("Post name of scholarship to add: ")
+        for a in scholarships:
+            if a["Name"] == name:
+                print("Scholarship already exists, please use 'del' command to delete it or 'edit' command to edit it.")
+                break
+        else:
+            if name == "quit" or name == "":
+                break
+            while True:
+                link = input("Post link of " + name + ": ")
+                if link != "":
+                    break
+                else:
+                    print("Please enter a valid link.")
+            
+            scholarships.append(dict([("Name", name), ("Link", link)]))
+            scholarshipspool.append(name)
+            f = open("Scholarships.txt", "a")
+            f.write(name + '\n' + link + '\n')
+            f.close()
+
+# List command : Lists all scholarships stored
+# Lists them in order that they were added
+def choice_list():
+    for i, a in enumerate(scholarships):
+        print(i + 1, ":", a["Name"], '-', a["Link"])
+
 # Importing/Creating 'Scholarship.txt' file
-try:
-    f = open("Scholarships.txt", "r")
-except (OSError, IOError):
-    f = open("Scholarships.txt", "x")
-    f.close()
-    f = open("Scholarships.txt", "r")
-finally:
-    
+def import_file():
+    try:
+        f = open("Scholarships.txt", "r")
+    except (OSError, IOError):
+        f = open("Scholarships.txt", "x")
+        f.close()
+        f = open("Scholarships.txt", "r")
+    return f
+
+
+# Main function
+def main():
+
+    f = import_file()
+
     # Create dict of scholarship names and links
     for i, a in enumerate(f):
         if i % 2 == 0:
-            scholarships.append(dict(("Name", a.rstrip('\n'))))
+            scholarships.append(dict({"Name" : a.rstrip('\n')}))
         else:
             scholarships[-1]["Link"] = a.rstrip('\n')
     f.close()
@@ -36,58 +102,14 @@ finally:
     while True:
         selection = input("> ").lower()
 
-        # Random command : Picks a random scholarship from scholarshippool and adds it to scholarshiptrashpool
-        # If there are no scholarships left in the pool, reset scholarshipspool and clear scholarshiptrashpool
         if selection == "random":
-            if len(scholarshipspool) == 0:
-                scholarshipspool = scholarshipstrashpool.copy()
-                scholarshipstrashpool.clear()
-            
-            try:
-                scholarshippicked = random.choice(scholarshipspool)
-            except IndexError:
-                print("Error: List of scholarships is empty, add scholarships first!")
-            else:
-                scholarshipstrashpool.append(scholarshippicked)
-                scholarshipspool.remove(scholarshippicked)
-                for a in scholarships:
-                    if a["Name"] == scholarshippicked:
-                        print(scholarshippicked, "-", a["Link"])
-                        break
-                else:
-                    print(scholarshippicked)
+            choice_random()
         
-        # Add command : Add scholarships name and link
-        # Must enter a unique scholarship with a non-empty link
-        # Stores it in 'Scholarship.txt'
         elif selection == "add":
-            while True:
-                name = input("Post name of scholarship to add: ")
-                for a in scholarships:
-                    if a["Name"] == name:
-                        print("Scholarship already exists, please use 'del' command to delete it or 'edit' command to edit it.")
-                        break
-                else:
-                    if name == "quit" or name == "":
-                        break
-                    while True:
-                        link = input("Post link of " + name + ": ")
-                        if link != "":
-                            break
-                        else:
-                            print("Please enter a valid link.")
-                    
-                    scholarships.append(dict([("Name", name), ("Link", link)]))
-                    scholarshipspool.append(name)
-                    f = open("Scholarships.txt", "a")
-                    f.write(name + '\n' + link + '\n')
-                    f.close()
+            choice_add()
         
-        # List command : Lists all scholarships stored
-        # Lists them in order that they were added
         elif selection == "list":
-            for i, a in enumerate(scholarships):
-                print(i + 1, ":", a["Name"], '-', a["Link"])
+            choice_list()
         
         # Commands yet to be added
         elif selection == "del" or selection == "edit":
@@ -100,5 +122,7 @@ finally:
         # Invalid command aka any command not recognized
         else:
             print("INVALID OPTION")
-        
 
+
+if __name__ == "__main__":
+    main()
